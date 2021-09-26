17d6c92215ff36f391508b917ccb875e18ecbeeb
Robert S Cohn <robert.s.cohn@intel.com>
2021-09-26 11:30:51 -0400

    enable scalapack sample

--- repo/packages/oneapi-test-basic/package.py
+++ repo/packages/oneapi-test-basic/package.py
@@ -30,6 +30,7 @@ class OneapiTestBasic(Package):
         'ippcp',
         'mkl',
         'mpi',
+        'scalapack',
         'sycl',
         'tbb',
         'vpl',
@@ -68,12 +69,9 @@ class OneapiTestBasic(Package):
                     targets.append(
                         f'MPI_PREFIX={self.spec["mpi"].prefix}/mpi/latest'
                     )
+                if c in ['scalapack', 'mkl', 'mpi']:
                     targets.append(
-                        f'MPI_LD_FLAGS={self.spec["mpi"].libs.ld_flags}'
-                    )
-                if c == 'mkl':
-                    targets.append(
-                        f'MKL_LD_FLAGS={self.spec["blas"].libs.ld_flags}'
+                        f'{c.upper()}_LD_FLAGS={self.spec[c].libs.ld_flags}'
                     )
                 targets.append(f'{c}-sample.out')
         make(
